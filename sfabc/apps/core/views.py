d425ab0003d6f6753e140fc87c351b3b156eb3c5
TonyyyB <tony.beaujouan2@gmail.com>
2025-09-30 16:05:58 +0200

    Début de la base, avec bootstrap5

--- sfabc/apps/core/views.py
+++ sfabc/apps/core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import *
 
 # Create your views here.
+class HomeView(TemplateView):
+    template_name = "base.html"
+
+    def post(self, request, **kwargs):
+        return render(request, self.template_name)
--- sfabc/sfabc/settings.py
+++ sfabc/sfabc/settings.py
@@ -37,10 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "bootstrap5",
     
     "apps.core",
     "apps.products",
-    "apps.reviews"
+    "apps.reviews",
 ]
 
 MIDDLEWARE = [
@@ -118,9 +119,32 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]   # fichiers statiques dans /static/
+STATIC_ROOT = BASE_DIR / "staticfiles"     # collectstatic en prod
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            BASE_DIR / "sfabc" / "templates",
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
