86f3e3ac63f9426953b50c7dd008c824e7453021
TonyyyB <tony.beaujouan2@gmail.com>
2025-10-01 17:41:13 +0200

    Ajout du début du header

--- sfabc/apps/core/views.py
+++ sfabc/apps/core/views.py
@@ -2,8 +2,5 @@ from django.shortcuts import render
 from django.views.generic import *
 
 # Create your views here.
-class HomeView(TemplateView):
-    template_name = "base.html"
-
-    def post(self, request, **kwargs):
-        return render(request, self.template_name)
+def home(request):
+    return render(request, 'base.html', {'menus': {'Chaise': {'Chaise en bois': 'chair'}, 'Tables': {'Table en bois': 'table'}, 'Canapés': {'Canapé en cuir': 'sofa'}}})
