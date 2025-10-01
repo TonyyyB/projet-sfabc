ce61d552683516bbc5f128341b8df9913b1a5513
varuniv <sargis.vardanyan@etu.univ-orleans.fr>
2025-10-01 16:27:40 +0200

    creationn de model Famille et produit

--- sfabc/apps/products/models.py
+++ sfabc/apps/products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Famille(models.Model):
+
+    id_famille = models.AutoField(primary_key=True)
+    nom_famille = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nomFamille
+    
+
+class Produit(models.Model):
+    
+    id_produit = models.AutoField(primary_key=True)
+    type_produit = models.CharField(max_length=200)
+    prix_produit = models.DecimalField(max_digits=10, decimal_places=2)
+    description_produit = models.TextField()
+    # Relation CIF : chaque produit appartient à 1 famille (0,N côté famille 1,1 côté produit)→
+    famille = models.ForeignKey(Famille, on_delete=models.CASCADE, related_name="produits",null=True, blank=True)
