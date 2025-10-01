982c3e131e9d3ab5c84255b2b09d25acd151f3f3
Kracocks <voivenelromain@gmail.com>
2025-10-01 17:56:04 +0200

    corrections problemes

--- sfabc/apps/core/models.py
+++ sfabc/apps/core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class A_Propos(models.model):
+class A_Propos(models.Model):
     id_ap = models.AutoField(primary_key=True)
     titre_ap = models.CharField(max_length=1000)
     description_ap = models.CharField()
@@ -23,7 +23,7 @@ class Site(models.Model):
     id = models.AutoField(primary_key=True)
     couleur = models.CharField(max_length=7) # exemple #000000
     police = models.CharField(max_length=200)
-    logo = models.ForeignKey(Image, related_name="image_site")
+    logo = models.ForeignKey(Image, on_delete=models.CASCADE, related_name="image_site")
 
     def __str__(self):
         return f"<Site {self.couleur}, {self.police}>"
