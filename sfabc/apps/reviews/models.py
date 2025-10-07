cd212a0f5ae222b29cd4911af44dca533aa183aa
Kracocks <voivenelromain@gmail.com>
2025-10-07 15:19:45 +0200

    modif emplacement model

--- sfabc/apps/reviews/models.py
+++ sfabc/apps/reviews/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+NOTES = {
+    "1 étoile" :  1,
+    "2 étoiles" : 2,
+    "3 étoiles" : 3,
+    "4 étoiles" : 4,
+    "5 étoiles" : 5,
+}
+
+class Note(models.Model):
+    id_note = models.AutoField(primary_key=True)
+    pseudonyme = models.CharField(max_length=100)
+    valeur = models.IntegerField(max_length=1, choices=NOTES)
+    message = models.CharField()
+
+    def __str__(self):
+        return f"{self.pseudonyme} : {self.valeur}"
