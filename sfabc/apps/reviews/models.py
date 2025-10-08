9dc63b97eed9effbba7ce2eef7a759415700b44d
Kracocks <voivenelromain@gmail.com>
2025-10-08 17:58:04 +0200

    ajout de tests pour note

--- sfabc/apps/reviews/models.py
+++ sfabc/apps/reviews/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 NOTES = {
-    "1 étoile" :  1,
-    "2 étoiles" : 2,
-    "3 étoiles" : 3,
-    "4 étoiles" : 4,
-    "5 étoiles" : 5,
+    1 : "1",
+    2 : "2",
+    3 : "3",
+    4 : "4",
+    5 : "5"
 }
 
 class Note(models.Model):
