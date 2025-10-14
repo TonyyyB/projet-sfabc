7196a4af176f2d7e5831db8480423ee8b68cef1f
TonyyyB <tony.beaujouan2@gmail.com>
2025-10-14 15:04:55 +0200

    Ajout de bootstrap dans le requirements et model dans la page admin

--- sfabc/apps/core/admin.py
+++ sfabc/apps/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from apps.core.models import A_Propos, Image, Site
 
 # Register your models here.
+admin.site.register(A_Propos)
+admin.site.register(Image)
+admin.site.register(Site)
