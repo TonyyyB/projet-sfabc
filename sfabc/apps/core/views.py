3bf77c8ab9f11f67e021f4e40aa1efee91e011b7
Kracocks <voivenelromain@gmail.com>
2025-10-15 16:38:26 +0200

    ajout contenu page a propos

--- sfabc/apps/core/views.py
+++ sfabc/apps/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import *
+from apps.core.models import A_Propos
 
 # Create your views here.
 def home(request):
@@ -21,3 +22,18 @@ def home(request):
         'Meubles de salle de bain': {'Meuble sous lavabo': 'bathroom_vanity', 'Armoire de salle de bain': 'bathroom_cabinet'}, 
         'Meubles multifonctions': {'Canapé-lit': 'sofa_bed', 'Table extensible': 'extendable_table'}, 
         'Meubles sur mesure': {'Meuble TV sur mesure': 'custom_tv_stand', 'Bibliothèque sur mesure': 'custom_bookcase'}}})
+
+
+class AProposView(ListView):
+    model = A_Propos
+    context_object_name = "infos"
+    template_name = "pages/about.html"
+
+
+    def get_queryset(self):
+        return A_Propos.objects.order_by("ordre_ap")
+
+
+    def get_context_data(self, **kwargs):
+        context = super(AProposView, self).get_context_data(**kwargs)
+        return context
--- sfabc/sfabc/context_processors.py
+++ sfabc/sfabc/context_processors.py
@@ -1,7 +1,6 @@
 from apps.core.models import Site
 def style_processor(request):
     site = Site.objects.all().first()
-    print(site.logo.image.url)
     style = ":root{"
     style += "--bs-body-bg: #f6f2e8 !important;"
     style += "}"
