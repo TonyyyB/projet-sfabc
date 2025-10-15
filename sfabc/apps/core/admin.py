4d186fa668f97c7913faa324329875ef64aab64d
TonyyyB <tony.beaujouan2@gmail.com>
2025-10-15 13:24:30 +0200

    Ajout du model de core dans la page admin, et ajout des urls pour les medias

--- sfabc/apps/core/admin.py
+++ sfabc/apps/core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from apps.core.models import A_Propos, Image, Site
+from apps.core.models import A_Propos, Image, Image_AP, Site
 
 # Register your models here.
 admin.site.register(A_Propos)
 admin.site.register(Image)
 admin.site.register(Site)
+admin.site.register(Image_AP)
