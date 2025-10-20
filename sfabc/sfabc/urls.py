81b5bec19ae4b90b4438425f672cb30827d56e92
TonyyyB <tony.beaujouan2@gmail.com>
2025-10-20 11:41:41 +0200

    Déplacement du menus dans le context_processor + merge des migrations

--- sfabc/sfabc/urls.py
+++ sfabc/sfabc/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("apps.core.urls", namespace="core")),
     path("produits/", include("apps.products.urls", namespace="products")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
