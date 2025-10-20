34abdc957ba5325d6dc0e9e12a4a5122dd31a386
TonyyyB <tony.beaujouan2@gmail.com>
2025-10-20 11:41:41 +0200

    Déplacement du menus dans le context_processor + merge des migrations

--- sfabc/apps/core/views.py
+++ sfabc/apps/core/views.py
@@ -4,15 +4,5 @@ from apps.products.models import *
 from django.db.models import Prefetch
 
 # Create your views here.
-class Home(ListView):
-    model = Produit
-    template_name = "pages/home.html"
-    context_object_name = "produits_moment"
-    
-    def get_queryset(self):
-        return Produit.objects.filter(is_produit_du_moment = True).prefetch_related(Prefetch("images_produit", queryset=Image_Produit.objects.filter(is_produit_du_moment = True).select_related("image"), to_attr='images_list'))
-    
-    def get_context_data(self, **kwargs):
-        context = super(Home, self).get_context_data(**kwargs)
-        context['title'] = "Découvrez mes produits"
-        return context
+def home(request):
+    return render(request, 'pages/home.html')
