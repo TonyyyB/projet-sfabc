ccbc0d6f9dc1e01bb2ce28e126bebbbbca9bbb95
varuniv <sargis.vardanyan@etu.univ-orleans.fr>
2025-10-21 11:16:03 +0200

    creation de contact part 1

--- sfabc/apps/core/urls.py
+++ sfabc/apps/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import *
+from apps.core.views import *
 
 app_name = "core"
 
 urlpatterns = [
     path("", Home.as_view(), name="home"),
-    path("a_propos/", AProposView.as_view(), name="a_propos")
+    path("a_propos/", AProposView.as_view(), name="a_propos"),
+    path("contact/", ContactView.as_view(), name="contact"),
 ]
