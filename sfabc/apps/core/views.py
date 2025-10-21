62df4f183dfa6adeab6117866802863171546ecd
clairedeneau <claire.deneau@etu.univ-orleans.fr>
2025-10-21 10:09:33 +0200

    Vue accueil + ptit ajout dans products/models.py

--- sfabc/apps/core/views.py
+++ sfabc/apps/core/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.views.generic import *
+from apps.products.models import *
+from django.db.models import Prefetch
 
 # Create your views here.
 def home(request):
     return render(request, 'pages/home.html')
+
+class Home(ListView):
+    model = Produit
+    template_name = "sfabc/templates/pages/home.html"
+    context_object_name = "produits_moment"
+    
+    def get_queryset(self):
+        return Produit.objects.filter(is_produit_du_moment = True).prefetch_related(Prefetch("image", queryset=Image_Produit.objects.select_related("produit").filter(is_produit_du_moment = True)))
+    
+    def get_context_data(self, **kwargs):
+        context = super(Home, self).get_context_data(**kwargs)
+        context['titre'] = "Découvrez mes produits"
+        return context
