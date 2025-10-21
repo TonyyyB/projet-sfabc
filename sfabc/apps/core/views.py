83fb54acc216f1169048b1c663f9de4859ce3f4b
varuniv <sargis.vardanyan@etu.univ-orleans.fr>
2025-10-21 11:16:03 +0200

    creation de contact part 1

--- sfabc/apps/core/views.py
+++ sfabc/apps/core/views.py
@@ -8,6 +8,10 @@ from django.core.mail import send_mail
 from django.shortcuts import redirect
 
 
+from .forms import ContactForm
+from django.core.mail import send_mail
+from django.shortcuts import redirect
+
 # Create your views here.
 class ContactView(FormView):
     template_name = 'pages/contact.html'
