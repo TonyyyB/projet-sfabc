b27abb8ad4cbe4ce384e60888a037025f995d7c1
clairedeneau <claire.deneau@etu.univ-orleans.fr>
2025-10-22 09:54:18 +0200

    Affichage des images + débuggage

--- sfabc/apps/products/admin.py
+++ sfabc/apps/products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from apps.products.models import *
 
 # Register your models here.
+admin.site.register(Famille)
+admin.site.register(Image_Produit)
+admin.site.register(Produit)
