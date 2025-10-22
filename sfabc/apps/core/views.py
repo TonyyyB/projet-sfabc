851ea8d6fa72e8a5e4f90b2ad9896bff0cad0495
clairedeneau <claire.deneau@etu.univ-orleans.fr>
2025-10-22 09:54:18 +0200

    Affichage des images + débuggage

--- sfabc/apps/core/views.py
+++ sfabc/apps/core/views.py
@@ -6,11 +6,11 @@ from django.db.models import Prefetch
 # Create your views here.
 class Home(ListView):
     model = Produit
-    template_name = "sfabc/templates/pages/home.html"
+    template_name = "pages/home.html"
     context_object_name = "produits_moment"
     
     def get_queryset(self):
-        return Produit.objects.filter(is_produit_du_moment = True).prefetch_related(Prefetch("image", queryset=Image_Produit.objects.select_related("produit").filter(is_produit_du_moment = True)))
+        return Produit.objects.filter(is_produit_du_moment = True).prefetch_related(Prefetch("images_produit", queryset=Image_Produit.objects.filter(is_produit_du_moment = True).select_related("image"), to_attr='images_list'))
     
     def get_context_data(self, **kwargs):
         context = super(Home, self).get_context_data(**kwargs)
