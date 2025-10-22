c8f4a5a1ec2527fa6a369164627d56f5318bfada
TonyyyB <tony.beaujouan2@gmail.com>
2025-10-22 10:41:11 +0200

    Avancée sur la page d'accueil avec images

--- sfabc/apps/products/migrations/0001_initial.py
+++ sfabc/apps/products/migrations/0001_initial.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('core', '0002_alter_site_police'),
+        ('core', '0001_initial'),
     ]
 
     operations = [
