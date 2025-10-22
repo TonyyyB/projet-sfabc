dd6b3b9c84bcedfbdf8fcb98b9c40e810e07c7e1
clairedeneau <claire.deneau@etu.univ-orleans.fr>
2025-10-22 09:54:18 +0200

    Affichage des images + débuggage

--- sfabc/apps/products/models.py
+++ sfabc/apps/products/models.py
@@ -15,10 +15,13 @@ class Produit(models.Model):
     
     id_produit = models.AutoField(primary_key=True)
     nom_produit = models.CharField(max_length=200)
-    prix_produit = models.DecimalField(max_digits=10, decimal_places=2)
+    prix_produit = models.DecimalField(max_digits=10, decimal_places=2, null = True, blank = True)
     description_produit = models.TextField()
     is_produit_du_moment = models.BooleanField(default=False)
     # Relation CIF : chaque produit appartient à 1 famille (0,N côté famille 1,1 côté produit)→
+
+    def __str__(self):
+        return f"Produit: {self.nom_produit}"
 
 class Image_Produit(models.Model):
     
