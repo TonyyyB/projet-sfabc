210c5e7cf6e42f3e763e5ce364b28027434b264d
TonyyyB <tony.beaujouan2@gmail.com>
2025-10-22 14:08:57 +0200

    Début du détail

--- sfabc/apps/products/urls.py
+++ sfabc/apps/products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 
+from apps.products.views import DetailProduitView
+
 app_name = "products"
 
 urlpatterns = [
-    #TODO
+    path("<int:pk>/", DetailProduitView.as_view(), name="detail_produit"),
     path("<int:pk>/avis/", include("apps.reviews.urls", namespace="reviews")),
 ]
