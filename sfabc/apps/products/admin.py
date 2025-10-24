3f8367ec3ff206f14740c6fc54f8fd892459caac
Kracocks <voivenelromain@gmail.com>
2025-10-24 10:13:41 +0200

    ajout page liste produits

--- sfabc/apps/products/admin.py
+++ sfabc/apps/products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from apps.products.models import Famille, Produit, Image_Produit
 
 # Register your models here.
+admin.site.register(Famille)
+admin.site.register(Produit)
+admin.site.register(Image_Produit)
