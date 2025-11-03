bd3b928249ac4eec307a17979ca99ee4cbb54476
TonyyyB <tony.beaujouan2@gmail.com>
2025-11-03 12:18:07 +0100

    Fix du rebase

--- sfabc/apps/core/views.py
+++ sfabc/apps/core/views.py
@@ -29,8 +29,13 @@ class Home(ListView):
     context_object_name = "produits_moment"
     
     def get_queryset(self):
-        return Produit.objects.filter(is_produit_du_moment = True).prefetch_related(Prefetch("images_produit", queryset=Image_Produit.objects.filter(is_produit_du_moment = True).select_related("image"), to_attr='images_list'))
-    
+        return Produit.objects.filter(is_produit_du_moment=True).prefetch_related(
+            Prefetch(
+            "images", 
+            queryset=Image_Produit.objects.filter(is_produit_du_moment=True),
+            to_attr='images_list'
+            )
+        )
     def get_context_data(self, **kwargs):
         context = super(Home, self).get_context_data(**kwargs)
         context['title'] = "Découvrez mes produits"
