025678f36c00e0f701aa127abf23694dca49eee6
varuniv <sargis.vardanyan@etu.univ-orleans.fr>
2025-11-03 11:22:25 +0100

    core site marche pas jsp pk

--- sfabc/apps/core/urls.py
+++ sfabc/apps/core/urls.py
@@ -6,6 +6,7 @@ app_name = "core"
 urlpatterns = [
     path("", Home.as_view(), name="home"),
     path("contact/", ContactView.as_view(), name="contact"),
-    path("a_propos/", AProposView.as_view(), name="a_propos")
+    path("a_propos/", AProposView.as_view(), name="a_propos"),
+    path("services/", ServiceView.as_view(), name="services"),
 
 ]
