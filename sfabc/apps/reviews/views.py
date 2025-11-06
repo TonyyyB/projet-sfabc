7974e74e5d70283908e2df5ad0fa9797a9a011f0
clairedeneau <claire.deneau@etu.univ-orleans.fr>
2025-11-06 12:22:10 +0100

    Pagination propre + étoiles

--- sfabc/apps/reviews/views.py
+++ sfabc/apps/reviews/views.py
@@ -9,3 +9,16 @@ class ReviewListView(SortableListView):
     allowed_sort_fields = {'valeur': {'default_direction': '-','verbose_name': 'Note'},'date': {'default_direction': '-','verbose_name': 'Publié le'}}
     default_sort_field = 'date'
     paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        context = super(SortableListView,
+                        self).get_context_data(**kwargs)
+        context['current_sort_query'] = self.get_sort_string()
+        context['current_querystring'] = self.get_querystring()
+        context['sort_link_list'] = self.sort_link_list
+        context['title'] = "Avis"
+        return context
+
+    def get_queryset(self):
+        qs = Avis.objects.filter(produit=self.kwargs['pk']).order_by(self.sort)
+        return qs
