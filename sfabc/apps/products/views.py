df204d4f0f0f8b911e19b2987d4e74c7972ee379
Kracocks <voivenelromain@gmail.com>
2025-11-07 09:26:04 +0100

    ajout pagination

--- sfabc/apps/products/views.py
+++ sfabc/apps/products/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import *
 from django.db.models import Prefetch
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import *
+
+
 
 # Create your views here.
 class ProduitListView(ListView):
     model = Produit
     template_name = "pages/liste_produits.html"
     context_object_name = "produits"
-
 
     def get_queryset(self):
         query = self.request.GET.get('search')
@@ -26,6 +28,22 @@ class ProduitListView(ListView):
                 p.img_affichage = p.images.all()[0]
             else:
                 p.img_affichage = None
+        
+        # La pagination
+        default_page = 1
+        page = self.request.GET.get('page', default_page)
+
+        items_per_page = 24
+        paginator = Paginator(context["produits"], items_per_page)
+
+        try:
+            items_page = paginator.page(page)
+        except PageNotAnInteger:
+            items_page = paginator.page(default_page)
+        except EmptyPage:
+            items_page = paginator.page(paginator.num_pages)
+        context["items_page"] = items_page
+
         return context
 
 
