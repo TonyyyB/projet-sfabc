d862961e1ecd8e20c8ffb77b6f6841c76f8522fa
Kracocks <voivenelromain@gmail.com>
2025-12-03 18:03:41 +0100

    modification nom fichier

--- sfabc/apps/core/tests/Model/tests_Image_Site_Model.py
+++ sfabc/apps/core/tests/Model/tests_Image_Site_Model.py
@@ -14,7 +14,7 @@ class ImageModelTest(TestCase):
             os.remove(self.image.image.path)
 
 
-    def test_image_update(self):
+    def test_image_site_update(self):
         self.image.image = SimpleUploadedFile(name="image_test1.png", content=b'', content_type="image/png")
         self.image.save()
 
@@ -23,6 +23,6 @@ class ImageModelTest(TestCase):
         self.assertEqual(str(self.image), "image_test1.png")
 
 
-    def test_image_delete(self):
+    def test_image_site_delete(self):
         self.image.delete()
         self.assertEqual(Image_Site.objects.count(), 0)
