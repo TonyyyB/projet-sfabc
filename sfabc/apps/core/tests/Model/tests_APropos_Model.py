5d0bf57504b2f96ef6487287da231951352fbf6e
Kracocks <voivenelromain@gmail.com>
2025-12-03 17:30:20 +0100

    réglage tests a_propos

--- sfabc/apps/core/tests/Model/tests_APropos_Model.py
+++ sfabc/apps/core/tests/Model/tests_APropos_Model.py
@@ -3,24 +3,30 @@ from apps.core.models import A_Propos
 
 class AProposModelTest(TestCase):
     def setUp(self):
-        self.ap = A_Propos.objects.create(titre_ap="titre test a propos", description_ap="description test a propos")
+        self.ap = A_Propos.objects.create(titre_ap="titre test a propos", description_ap="description test a propos", ordre_ap=1)
 
-    def test_categorie_create(self):
+    def test_ap_create(self):
         self.assertEqual(self.ap.titre_ap, "titre test a propos")
         self.assertEqual(self.ap.description_ap, "description test a propos")
+        self.assertEqual(self.ap.ordre_ap, 1)
+
 
     def test_string_repr(self):
         self.assertEqual(str(self.ap), "titre test a propos")
 
-    def test_categorie_update(self):
+
+    def test_ap_update(self):
         self.ap.titre_ap = "titre2 test a propos"
         self.ap.description_ap = "description2 test a propos"
+        self.ap.ordre_ap = 2
         self.ap.save()
 
-        update_categ = A_Propos.objects.get(idCat=self.ap.id_ap)
+        update_categ = A_Propos.objects.get(id_ap=self.ap.id_ap)
         self.assertEqual(update_categ.titre_ap, "titre2 test a propos")
         self.assertEqual(update_categ.description_ap, "description2 test a propos")
+        self.assertEqual(update_categ.ordre_ap, 2)
 
-    def test_categorie_delete(self):
+
+    def test_ap_delete(self):
         self.ap.delete()
         self.assertEqual(A_Propos.objects.count(), 0)
