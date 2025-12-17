1297571d75fd2823cab6547d742cc49977e7a123
clairedeneau <claire.deneau@etu.univ-orleans.fr>
2025-12-17 09:06:43 +0100

    Continuation style avis

--- sfabc/apps/reviews/views.py
+++ sfabc/apps/reviews/views.py
@@ -6,7 +6,7 @@ class ReviewListView(SortableListView):
     model = Avis
     template_name = "reviews/reviews.html"
     context_object_name = "reviews"
-    allowed_sort_fields = {'valeur': {'default_direction': '-','verbose_name': 'Note'},'date': {'default_direction': '-','verbose_name': 'Publié le'}}
+    allowed_sort_fields = {'valeur': {'default_direction': '+','verbose_name': 'Note'},'date': {'default_direction': '-','verbose_name': 'Publié le'}}
     default_sort_field = 'date'
     paginate_by = 5
 
