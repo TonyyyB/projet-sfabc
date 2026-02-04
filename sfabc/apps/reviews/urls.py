73fa40b6088740906c942889e149d8c70d38aa57
TonyyyB <tony.beaujouan2@gmail.com>
2026-02-04 16:26:05 +0100

    Ajout de la page admin des reviews

--- sfabc/apps/reviews/urls.py
+++ sfabc/apps/reviews/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import ReviewListView, add_review
+from .views import ReviewListView, add_review, add_reply
 
 app_name = "reviews"
 
 urlpatterns = [
     path("add/", add_review, name="add_review"),
+    path("reply/<int:avis_id>/", add_reply, name="add_reply"),
     path("", ReviewListView.as_view(), name="liste_avis"),
 ]
